75d93b45403e124f9ff24cbb562a6b4b441a79f7
Victor Costan <costan@gmail.com>
2009-06-04 17:07:23 -0400

    With model annotations.

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+# Schema version: 20090604175814
+#
+# Table name: results
+#
+#  id         :integer         not null, primary key
+#  query_id   :integer
+#  record_id  :integer
+#  score      :integer
+#  start_time :integer
+#  end_time   :integer
+#  price      :integer
+#  layovers   :integer
+#  start_city :string(255)
+#  end_city   :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Result < ActiveRecord::Base
   belongs_to :query
 end
